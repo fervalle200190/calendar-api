fac596421902b61eeea6a675a2c383a2ee714db8
Fernando Vallejo <fervalle200190@gmail.com>
2023-01-20 15:23:15 -0400

    first commit

---
+++ helpers/isDate.js
@@ -0,0 +1,13 @@
+const moment = require("moment")
+
+const isDate = (value) => {
+     if (!value) return false
+
+     const fecha = moment(value)
+     if (!fecha.isValid()) return false
+     return true
+}
+
+module.exports = {
+     isDate,
+}
